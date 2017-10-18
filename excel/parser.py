5c1b64742c3a543ac2d40adb20f6047c497d1c20
lesroutes2014 <lesroutes2014@gmail.com>
2017-10-18 10:44:19 -0400

    Started parsing function and template, data, mapping file

--- excel/parser.py
+++ excel/parser.py
@@ -1,2 +1,10 @@
-def parse_excel_file(file_name, ):
-    pass
+from openpyxl import load_workbook
+
+
+def parse_excel_file(file_name, sheet, columns, first_row=0, last_row=None):
+    wb = load_workbook(filename=file_name, read_only=True)
+    ws = wb[sheet]
+
+    for row in ws.rows:
+        for cell in row:
+            print(cell.value)
