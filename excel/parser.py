4b35e96c85b596e8e72037528341e55e83fd95bc
lesroutes2014 <lesroutes2014@gmail.com>
2017-10-19 19:36:04 -0400

    Added print function, few conversion functions and empty function finder

--- excel/parser.py
+++ excel/parser.py
@@ -1,10 +1,56 @@
+import sys
+from itertools import filterfalse
 from openpyxl import load_workbook
 
 
-def parse_excel_file(file_name, sheet, columns, first_row=0, last_row=None):
+def parse_excel_file(file_name, sheet_name, columns, first_row=0, last_row=None, remove_empty_lines=True):
     wb = load_workbook(filename=file_name, read_only=True)
-    ws = wb[sheet]
+    ws = wb[sheet_name]
 
-    for row in ws.rows:
+    data = []
+
+    for row in ws.iter_rows(row_offset=first_row):
+        row_data = []
         for cell in row:
-            print(cell.value)
+            row_data.append(to_str(cell.value))
+
+        if remove_empty_lines and empty(row_data):
+            pass
+        else:
+            data.append(row_data)
+
+    return data
+
+
+def print_table(data_table):
+    for i in range(len(data_table)):
+        for j in range(len(data_table[i])):
+            print(data_table[i][j] + " ", end='')
+        print("")
+
+
+def to_str(cell_content):
+    if cell_content is None:
+        return ""
+    else:
+        return cell_content
+
+
+def empty(line):
+    if not all(cell == "" for cell in line):
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    file_name = sys.argv[1]
+    sheet_name = sys.argv[2]
+
+    columns = []
+
+    data_table = parse_excel_file(file_name, sheet_name, columns)
+
+    # print_table(data_table)
+
+    remove_empty_lines(data_table)
